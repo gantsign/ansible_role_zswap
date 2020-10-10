efbc8e7a594f75009e8647ed1a1d9ff9ddc4d475
John Freeman <john.freeman.opensource@gmail.com>
2020-10-10 20:27:51 +0100

    Updated Molecule to 3.0.8 (#12)
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,12 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
-
-
 def test_zswap(host):
     grub = host.file('/etc/default/grub')
     assert grub.exists
